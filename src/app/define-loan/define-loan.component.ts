a88b02a6fa7c7859d8b0338e9a8adb09b071173f
Rodnier Borrego <rodnierbc89@gmail.com>
2018-09-19 23:05:40 -0700

    Added some logic to createPaymentSchedule() function

--- src/app/define-loan/define-loan.component.ts
+++ src/app/define-loan/define-loan.component.ts
@@ -19,8 +19,20 @@ export class DefineLoanComponent implements OnInit {
 
   
 
-  createPaymentSchedule(loanAmount: string, installmentAmount: string, simpleInterestRate: string, installmentInterval: string) {
-    alert(this.percentage(1000, 50.02))
+  createPaymentSchedule(loanAmountP: string, installmentAmountP: string, simpleInterestRateP: string, installmentIntervalP: string) {
+    let amountToPay = Number(loanAmountP);
+    let installmentAmount= Number(installmentAmountP);
+    let simpleInterestRate = Number(simpleInterestRateP);
+    let installmentInterval = Number(installmentIntervalP);
+    
+    while(amountToPay != 0){
+      if(amountToPay - installmentAmount < 0){
+        installmentAmount = amountToPay;
+      }
+      let currentPay = installmentAmount + this.percentage(amountToPay, simpleInterestRate);
+      amountToPay = amountToPay - installmentAmount;
+      alert(currentPay);
+    }
     
   }
 
