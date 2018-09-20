e659e2a1dc8733056f4c944c63c5196022e184db
Rodnier Borrego <rodnierbc89@gmail.com>
2018-09-20 01:16:41 -0700

    create nextPaymentDate() function

--- src/app/define-loan/define-loan.component.ts
+++ src/app/define-loan/define-loan.component.ts
@@ -24,6 +24,7 @@ export class DefineLoanComponent implements OnInit {
     let installmentAmount= Number(installmentAmountP);
     let simpleInterestRate = Number(simpleInterestRateP);
     let installmentInterval = Number(installmentIntervalP);
+    let lastPaymentDate = this.date;
     
     while(amountToPay != 0){
       if(amountToPay - installmentAmount < 0){
@@ -31,18 +32,139 @@ export class DefineLoanComponent implements OnInit {
       }
       let currentPay = installmentAmount + this.percentage(amountToPay, simpleInterestRate);
       amountToPay = amountToPay - installmentAmount;
-      alert(currentPay);
+      lastPaymentDate = this.nextPaymentDate(lastPaymentDate, installmentInterval);
+      alert(lastPaymentDate.month+"-"+lastPaymentDate.day+"-"+lastPaymentDate.year+"---------->"+currentPay);
+
     }
     
   }
 
 
-  leap(year: number) {
-    return new Date(year, 1, 29).getMonth() === 1
-  }
-  percentage(num: number, per: number)
-{
+leap(year: number){
+  return new Date(year, 1, 29).getMonth() === 1
+}
+percentage(num: number, per: number){
   return (num/100)*per;
 }
+
+nextPaymentDate(lastPaymentDate: DateFormat, installmentInterval: number){
+   let nextPaymentDate = lastPaymentDate;
+     switch(lastPaymentDate.month){
+      case 1:
+      case 3:
+      case 5:
+      case 7:
+      case 8:
+      case 10: {//casos para los meses que tienen 31 dias
+        if(installmentInterval == 1){//caso diario
+         if(lastPaymentDate.day == 31){//si es el ultimo dia del mes(31)
+           nextPaymentDate.month += 1;
+           nextPaymentDate.day = 1;
+         }
+         else nextPaymentDate.day += 1;
+        }
+        else if(installmentInterval == 2){//caso semanal
+          if(lastPaymentDate.day >= 24){// chequear si tenemos que cambiar al mes siguiente
+            nextPaymentDate.month += 1;
+            nextPaymentDate.day = lastPaymentDate.day -24;
+          }
+          else nextPaymentDate.day += 7;
+        }
+        else if(installmentInterval == 3){//caso mensual
+          nextPaymentDate.month += 1;         
+        }
+        break;
+       }
+       case 4:
+       case 6:
+       case 9:
+       case 11:{
+         if(installmentInterval == 1){
+          if(lastPaymentDate.day == 30){
+            nextPaymentDate.month += 1;
+            nextPaymentDate.day = 1;
+          }
+          else nextPaymentDate.day += 1;
+         }
+         else if(installmentInterval == 2){
+          if(lastPaymentDate.day >= 23){
+            nextPaymentDate.month += 1;
+            nextPaymentDate.day = lastPaymentDate.day -23;
+          }
+          else nextPaymentDate.day += 7;
+         }
+         else if(installmentInterval == 3){//caso mensual
+          nextPaymentDate.month += 1;         
+        }        
+        break;
+       }
+       case 12:{
+         if(installmentInterval == 1){
+          if(lastPaymentDate.day == 31){
+            nextPaymentDate.month = 1;
+            nextPaymentDate.day = 1;
+            nextPaymentDate.year += 1;
+          }
+          else nextPaymentDate.day += 1;
+         }
+         else if(installmentInterval == 2){//caso semanal
+          if(lastPaymentDate.day >= 24){// chequear si tenemos que cambiar al mes siguiente
+            nextPaymentDate.year += 1
+            nextPaymentDate.month = 1;
+            nextPaymentDate.day = lastPaymentDate.day -24;
+          }
+          else nextPaymentDate.day += 7;
+        }
+        else if(installmentInterval == 3){//caso mensual
+          nextPaymentDate.month = 1;
+          nextPaymentDate.year += 1;      
+        }         
+        break;
+       }
+       case 2:{
+         if(this.leap(lastPaymentDate.year)){
+           if(installmentInterval == 1){
+            if(lastPaymentDate.day == 29){
+              nextPaymentDate.month += 1;
+              nextPaymentDate.day = 1;
+            }
+            else nextPaymentDate.day += 1;
+           }
+           else if(installmentInterval == 2){
+            if(lastPaymentDate.day >= 22){// chequear si tenemos que cambiar al mes siguiente
+              nextPaymentDate.month += 1;
+              nextPaymentDate.day = lastPaymentDate.day -22;
+            }
+            else nextPaymentDate.day += 7;
+           }
+           else if(installmentInterval == 3){
+            nextPaymentDate.month += 1; 
+           }           
+           break;
+         }
+         else {
+          if(installmentInterval == 1){
+            if(lastPaymentDate.day == 28){
+              nextPaymentDate.month += 1;
+              nextPaymentDate.day = 1;
+            }
+            else nextPaymentDate.day += 1;
+           }
+           else if(installmentInterval == 2){
+            if(lastPaymentDate.day >= 21){// chequear si tenemos que cambiar al mes siguiente
+              nextPaymentDate.month += 1;
+              nextPaymentDate.day = lastPaymentDate.day -21;
+            }
+            else nextPaymentDate.day += 7;
+           }
+           else if(installmentInterval == 3){
+            nextPaymentDate.month += 1; 
+           }           
+           break;
+         }      
+       }
+     }    
+   return nextPaymentDate;
+  }
 
 }
